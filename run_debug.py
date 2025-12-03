6055d359c46fedd1439178ec9bca93787fc8b485
Sharva Deodhar <sharva.deodhar@gmail.com>
2025-12-03 15:38:40 -0500

    finalized without debugger files

--- run_debug.py
+++
@@ -1,6 +0,0 @@
-import optimize
-params = {'consumers': 10, 'methods': 3, 'timesteps': 12, 'months_in_timestep': 3, 'emissions_grams': [578.4,0.653,82.8], 'costs_USD':[15,6,5], 'incomes':[25000,75000,125000,175000], 'probabilities':[0.442,0.347,0.145,0.066], 'percent_income':0.0005}
-res = optimize.optimize(params)
-print('RESULT KEYS:', list(res.keys()))
-print('consumer_incomes:', res.get('consumer_incomes'))
-print('num_opt_choices:', len(res.get('Optimal_Choices', {})))
